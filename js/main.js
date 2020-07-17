4f0328f09ff92460de7f199d7da7bc120c3d30e5
Yallovi <vivimoskalenko@yandex.ru>
2020-07-17 15:33:32 +0300

    Добавил два слайдера (блок production и feedback)

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ const popup = () => {
 });
 };
 popup();
+
+// slick slider production
+$('.production-slider__top').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.production-slider__bot'
+  });
+  $('.production-slider__bot').slick({
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_left"</div>',
+    nextArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_right"</div>',
+    asNavFor: '.production-slider__top',
+
+  });
+  // slick slider feedback
+  $('.feedback-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<div class="slider-arrow slider-arrow_feedback slider-arrow_left"</div>',
+    nextArrow: '<div class="slider-arrow slider-arrow_feedback slider-arrow_right"</div>',
+  });
